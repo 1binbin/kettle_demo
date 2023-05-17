e000051cd4f04a767ea3390cd5f2e00fe40c75a4
hongxiaobin <2812181610@qq.com>
2023-05-17 15:53:42 +0800

    feat: 修正错误

--- src/main/java/com/kettle/demo/controller/KettleController.java
+++ src/main/java/com/kettle/demo/controller/KettleController.java
@@ -6,9 +6,7 @@
  */
 package com.kettle.demo.controller;
 
-import java.io.File;
-import java.util.Map;
-
+import cn.hutool.core.io.FileUtil;
 import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.plugins.PluginFolder;
@@ -19,7 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import cn.hutool.core.io.FileUtil;
+import java.io.File;
+import java.util.Map;
 
 @RestController
 @RequestMapping("kettle")
@@ -38,15 +37,9 @@ public class KettleController {
 
     @RequestMapping("testKettle")
     public String testKettle() throws KettleException {
-
-        // 可以把文件放到resources目录下面，然后用hutool读取这文件
-        // File file = FileUtil.file("33.ktr");
-        // 这是我本地进行测试的，所以我把文件放到了桌面上了，写了根目录
         File file = FileUtil.file(
-                "C:/Users/Administrator/workspace/vsCode/XADPwdManage/files/ad9899a0dfcdeb07c464af8e487d52e6/tempfile.txt.ktr");
+                "E:\\1gziscas\\佛山智慧廉政\\kettle脚本\\sxcs.ktr");
         String path = file.getPath();
-        StepPluginType.getInstance().getPluginFolders().add(new PluginFolder(
-                "E:\\迅雷下载\\pdi-ce-8.2.0.0-342\\data-integration\\plugins\\steps\\pentaho-kafka-producer", false, true));
         // 初始化
         KettleEnvironment.init();
         // 加载文件
@@ -54,24 +47,10 @@ public class KettleController {
         Trans trans = new Trans(transMeta);
         // 放入参数，这里其实可以从数据库中取到
         // 如果没有参数，可以把这步忽略
-        trans.setParameterValue("stade", "2019-04-24");
         trans.prepareExecution(null);
         trans.startThreads();
         // 等待执行完毕
         trans.waitUntilFinished();
-
-        // // 初始化
-        // KettleEnvironment.init();
-        // // 加载文件
-        // File file = FileUtil.file("C:\\Users\\Y\\Desktop\\33.ktr");
-        // TransMeta transMeta = new
-        // TransMeta("C:\\Users\\Administrator\\Desktop\\deskTopFolder\\springboot_kettle.ktr");
-        // Trans trans = new Trans(transMeta);
-        // // 放入参数，这里其实可以从数据库中取到，如果没有参数，可以把这步忽略
-        // // trans.setParameterValue("stade", "2019-04-24");
-        // trans.prepareExecution(null);
-        // trans.startThreads();
-        // trans.waitUntilFinished();
         return "success";
     }
 
--- src/test/java/com/kettle/demo/DemoApplicationTests.java
+++ src/test/java/com/kettle/demo/DemoApplicationTests.java
@@ -1,13 +1,43 @@
 package com.kettle.demo;
 
+import cn.hutool.core.io.FileUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.pentaho.di.core.KettleEnvironment;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+
+@Slf4j
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() throws KettleException {
+        long begin = System.currentTimeMillis();
+
+
+        File file = FileUtil.file(
+                "D:\\桌面\\test.ktr");
+
+
+        String path = file.getPath();
+        // 初始化
+        KettleEnvironment.init();
+        // 加载文件
+        TransMeta transMeta = new TransMeta(path);
+        Trans trans = new Trans(transMeta);
+        // 放入参数，这里其实可以从数据库中取到
+        // 如果没有参数，可以把这步忽略
+        trans.prepareExecution(null);
+        trans.startThreads();
+        // 等待执行完毕
+        trans.waitUntilFinished();
+        long end = System.currentTimeMillis();
+        log.info("{}执行完毕，用时:{}ms", file.getName(), end - begin);
+    }
 
 }
